95193fea8052b1050381f611f184df5f6511cb28
Ngoc Trung <tylerthetylerthe@gmail.com>
2024-08-09 16:50:34 +0700

    09_08_2024

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import "./style.css";
-// import Products from "./components/products/Products";
 import ProductPage from "./pages/productpage/ProductPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 import PaymentPage from "./pages/PaymentPage/PaymentPage";
 import CartPage from "./pages/CartPage/CartPage";
 import DetailPage from "./pages/DetailPage/DetailPage";
 import SearchResults from "./components/SearchResult";
+
 export default function App() {
   return (
     <div>
-        <BrowserRouter>
+      <HashRouter>
         {/* <BreadcrumbsComponent /> */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<SearchResults/>}/>
-            <Route path="/products/" element={<ProductPage />} />
-            <Route path="/detail/:id" element={<DetailPage />} />
-            <Route path="/products/detail/:id" element={<DetailPage />} />
-            <Route path="/cart/" element={<CartPage />} />
-            <Route path="/contact/" element={<ContactPage />} />
-            {/* <Route path="/payment/" element={<PaymentPage />} /> */}
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchResults />} />
+          <Route path="/products" element={<ProductPage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+          <Route path="/products/detail/:id" element={<DetailPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          {/* <Route path="/payment" element={<PaymentPage />} /> */}
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
